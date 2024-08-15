4533b7106729a40950efb76c8de8f3d3844ae6ed
jacristi <jacristi0428@gmail.com>
2024-08-14 22:52:39 -0400

    feat: music track + small updates

--- entities/collectables/bug1.gd
+++ entities/collectables/bug1.gd
@@ -1,6 +1,12 @@
 extends Area2D
 
 
+
+func _ready() -> void:
+    pass
+    ### Get total bugs in level
+    ### on
+
 func _on_body_entered(area: CharacterBody2D):
     var bugs = get_tree().get_nodes_in_group("Bugs")
     if bugs.size() == 1:
