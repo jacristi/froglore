8f820b442ebd303e0d6d2d93f8863070c4c0328b
jacristi <jacristi0428@gmail.com>
2024-08-20 15:14:14 -0400

    feat: updated wter shader

--- entities/world/water.gd
+++ entities/world/water.gd
@@ -12,3 +12,7 @@ func _process(_delta: float) -> void:
 
 func _zoom_changed():
     material.set_shader_parameter("y_zoom", get_viewport_transform().get_scale().y)
+
+
+func _on_item_rect_changed() -> void:
+    material.set_shader_parameter("scale", scale)
