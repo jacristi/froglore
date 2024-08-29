b3cdd48dfd7c33e1ce4421217a89c6bc3a78df98
jacristi <jacristi0428@gmail.com>
2024-08-29 19:54:56 -0400

    feat: added potential scene switch trigger area

---
+++ entities/camera/scene_transition_area.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@export var go_to_level_key:= "N/A"
+
+
+func _on_body_entered(body: Node2D) -> void:
+    if go_to_level_key == "N/A": return
+    Events.go_to_level.emit(go_to_level_key)
