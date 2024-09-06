6793a1be66e020267b2fbce4b52805f9620007c0
jacristi <jacristi0428@gmail.com>
2024-09-06 00:21:46 -0400

    feat: dont show empty dialogue

--- ui/dialogue_sign.gd
+++ ui/dialogue_sign.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-@export_multiline var dialogue_text := 'No Text Defined'
+@export_multiline var dialogue_text := ''
 @export var show_sprite := true
 @export var show_once := false
 @onready var sprite_2d: Sprite2D = $Sprite2D
@@ -15,8 +15,8 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
     if show_once and has_shown: return
     has_shown = true
+    if dialogue_text == '': return
     Events.show_dialogue.emit(dialogue_text, 0)
-
 
 func _on_body_exited(body: Node2D) -> void:
     Events.hide_dialogue.emit()
