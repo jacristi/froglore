4bc709140dc1ab8bc3e23a24383e40fce634ffbe
jacristi <jacristi0428@gmail.com>
2025-08-27 16:48:18 -0400

    feat: controls menu

--- ui/pause_canvas.gd
+++ ui/pause_canvas.gd
@@ -4,25 +4,33 @@ var is_paused := false
 @onready var pause_texture: NinePatchRect = $PauseTexture
 @onready var animation_component: AnimationComponent = $PauseTexture/AnimationComponent
 @onready var resume_button: Button = %ResumeButton
+@onready var controls_texture: NinePatchRect = $ControlsTexture
+@onready var controls_animation_component: AnimationComponent = $ControlsTexture/ControlsAnimationComponent
 
 
 func _ready() -> void:
     Events.pause_pressed.connect(show_hide_pause_menu)
     pause_texture.hide()
     animation_component.close()
+    controls_texture.hide()
+    controls_animation_component.close()
 
 
 func show_hide_pause_menu():
     if is_paused:
         animation_component.close()
+        controls_animation_component.close()
         get_tree().paused = false
 
         await get_tree().create_timer(.15).timeout
         pause_texture.hide()
+        controls_texture.hide()
     else:
         resume_button.grab_focus()
         pause_texture.show()
         animation_component.open()
+        #controls_texture.show()
+        controls_animation_component.open()
         await get_tree().create_timer(.15).timeout
         get_tree().paused = true
 
@@ -32,11 +40,25 @@ func show_hide_pause_menu():
 
 func _on_resume_button_pressed() -> void:
     animation_component.close()
+    controls_animation_component.close()
     get_tree().paused = false
     is_paused = false
     await get_tree().create_timer(.15).timeout
     pause_texture.hide()
+    controls_texture.hide()
 
 
 func _on_exit_button_pressed() -> void:
     Events.try_exit_game.emit()
+
+
+func _on_controls_button_pressed() -> void:
+    print('pressed')
+    if controls_texture.visible:
+        controls_animation_component.close()
+        controls_texture.hide()
+        print('hide?')
+    else:
+        print('show')
+        controls_texture.show()
+        controls_animation_component.open()
