31558ce6e5cd836e6ff853cd64a43ced6a3a6dae
James Cristini <jamescristini@Jamess-MBP-140.lan>
2026-02-12 22:05:56 -0500

    feat: started menus

---
+++ ui/menus/menu.gd
@@ -0,0 +1,48 @@
+class_name Menu
+extends MarginContainer
+
+var is_active = true:
+    set(value):
+        if is_active == value: return
+        is_active = value
+
+
+var _menu_items: Dictionary[int, MenuItem]
+@onready var menu_item_list: VBoxContainer = $MenuItemList
+@onready var header: Label = $MenuItemList/Header
+
+var max_idx:= 1
+var selected_idx:= 0:
+    set(value):
+        if selected_idx == value: return
+        selected_idx = value
+        for k in _menu_items.keys():
+            _menu_items[k].is_selected = k == selected_idx
+
+var menu_item_scene = load("res://ui/menus/menu_item.tscn")
+@export var header_text: String = "Header"
+@export var menu_items_dict: Dictionary[String, String]
+
+func _ready() -> void:
+    """ """
+    header.text = header_text
+    var count = 0
+    for txt in menu_items_dict.keys():
+        var item: MenuItem = menu_item_scene.instantiate()
+        item.label_text = txt
+        menu_item_list.add_child(item)
+        _menu_items[count] = item
+        item.is_selected = count == 0
+        count += 1
+
+    max_idx = count-1
+
+
+func _process(delta: float) -> void:
+    if not is_active: return
+    if Input.is_action_just_pressed("up"):
+        selected_idx = clamp(selected_idx-1, 0, max_idx)
+    if Input.is_action_just_pressed("down"):
+        selected_idx = clamp(selected_idx+1, 0, max_idx)
+
+    pass
---
+++ ui/menus/menu_item.gd
@@ -0,0 +1,29 @@
+class_name MenuItem
+extends MarginContainer
+
+@onready var menu_item_label: Label = %MenuItemLabel
+
+@onready var left_selected_texture_rect:  TextureRect = %LeftSelectedTextureRect
+@onready var right_selected_texture_rect: TextureRect = %RightSelectedTextureRect
+
+@export var label_text: String
+
+var is_selected_texture =  load("res://assets/spritesheets/ui/spr-ui-selected-indicator.png")
+var not_selected_texture = load("res://assets/spritesheets/ui/spr-ui-notselected-indicator.png")
+
+
+var is_selected:= true:
+    set(value):
+        print('go')
+        if is_selected == value: return
+        is_selected = value
+        if is_selected:
+            left_selected_texture_rect.texture = is_selected_texture
+            right_selected_texture_rect.texture = is_selected_texture
+        else:
+            left_selected_texture_rect.texture = not_selected_texture
+            right_selected_texture_rect.texture = not_selected_texture
+
+func _ready() -> void:
+    is_selected = false
+    menu_item_label.text = label_text
